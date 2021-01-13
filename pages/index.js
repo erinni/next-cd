771da2c1e56de231122b7dd1015e5e34c6534b0d
erinni <andrea.caramanna@gmail.com>
2021-01-13 11:41:50 +0100

    altra piccola modifica

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          CIAO!!! to <a href="https://nextjs.org">Next.js!</a>
+          WELCOME to <a href="https://nextjs.org">MY Next.js!</a>
         </h1>
 
         <p className={styles.description}>
